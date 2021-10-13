5868c584241ee4e75f73d833ab2371f30ccc55af
ThierryWouters <thierry@thierrywouters.com>
2021-10-13 18:54:23 +0200

    Huiswerk Banana Security

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useContext} from 'react';
+import {Link, useHistory} from 'react-router-dom';
+import {AuthContext} from "../context/AuthContext";
 
 function SignIn() {
+
+    const {login, isAuth} = useContext(AuthContext);
+    console.log(isAuth)
   return (
     <>
       <h1>Inloggen</h1>
@@ -9,7 +13,7 @@ function SignIn() {
 
       <form>
         <p>*invoervelden*</p>
-        <button>Inloggen</button>
+        <button type="button" onClick={login}>Inloggen</button>
       </form>
 
       <p>Heb je nog geen account? <Link to="/signup">Registreer</Link> je dan eerst.</p>
